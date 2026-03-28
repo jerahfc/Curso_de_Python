f04f1fa7fb1fbb96220e9974867f53b20f521080
Jerah Fonseca Carvalho <jerah.fc@gmail.com>
2026-03-28 20:23:47 -0300

    ok

---
+++ Curso_Python/aula004_desafio002.py
@@ -0,0 +1,4 @@
+dia = input("Que dia você nasceu? ")
+mês = input("Qual seu mês de nasciento? ")
+ano = input("Qual ano você nasceu? ")
+print("Você nasceu no dia", dia, "de", mês, "de", ano,". Correto?")
