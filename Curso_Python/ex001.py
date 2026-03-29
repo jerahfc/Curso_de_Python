cc813c0f28cfe3c0994b2dc97f2355b17c957c5e
Jerah Fonseca Carvalho <jerah.fc@gmail.com>
2026-03-29 12:43:20 -0300

    ex002ok

--- Curso_Python/ex001.py
+++ Curso_Python/ex001.py
@@ -1,4 +1,8 @@
+#Crie um programa que escreva "Olá, Mundo!" na tela.
+
 print("Olá, Mundo!")
+
+#-------------------------Outra forma de fazer---------------------------------------------
 
 msg = "Olá, Mundo!"
 print(msg)
---
+++ Curso_Python/ex002.py
@@ -0,0 +1,9 @@
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+nome = input("Qual é o seu nome? " )
+print("É um prazer te conhecer, ", nome,)
+
+#-------------------------Outra forma de fazer---------------------------------------------
+
+nome = input("Qual é o seu nome? " )
+print("É um prazer te conhecer, {}!".format(nome))
