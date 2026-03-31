aea74d08fd34b653be0b0dd5637109dead333807
Jerah Fonseca Carvalho <jerah.fc@gmail.com>
2026-03-30 22:54:36 -0300

    ok

--- Curso_Python/aula006_tipos_primitivos_e_saida_de_dados.py
+++ Curso_Python/aula006_tipos_primitivos_e_saida_de_dados.py
@@ -5,8 +5,8 @@ float       # Números Reais ou números de ponto flutuante. ---> Ex: 4.5, 0.076
 bool        # Valores Lógicos ou Booleanos. ---> Ex: True, False
 str         # Valores Varactere ou String. ---> Ex: "Olá", "7.5", " "
 
-#print("A soma vale", S)
-#print("A soma vale, {}".format(S))
+# print("A soma vale", S)
+# print("A soma vale, {}".format(S))
 
 n1 = int(input("digite um valor: "))
 n2 = int(input("digite um valor: "))
@@ -18,4 +18,9 @@ print("A soma vale", s)
 n1 = int(input("digite um valor: "))
 n2 = int(input("digite outro valor: "))
 s = n1 + n2
-print("A soma entre ", n1, "e", n2, "é", s)
+# jeito muito longo ---> print("A soma entre ", n1, "e", n2, "é", s)
+
+# jeito antigo no Python 3.2 ---> print("A soma entre {} e {} vale {}".format(n1, n2, s))
+
+print(f"A soma entre {n1} e {n2} vale {s}")
+
