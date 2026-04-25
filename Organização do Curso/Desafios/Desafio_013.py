ff684f5a1a4f98edbbe36f7228baa0ff9b616839
Jerah Fonseca Carvalho <jerah.fc@gmail.com>
2026-04-25 18:02:03 -0300

    fiz o desafio 13

---
+++ Organização do Curso/Desafios/Desafio_013.py
@@ -0,0 +1,12 @@
+# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
+salario = float(input("Qual o seu salário? R$"))
+aumento = (15/100) * salario
+novo_salario = salario + aumento
+print(f"Seu novo salário com aumento é de R${novo_salario:.2f}")
+
+# ------------OUTRA FORMA DE FAZER--------------------------------------------------------------------------------------
+
+salario = float(input("Qual o seu salário? R$"))
+novo_salario = salario * 1.15
+print(f"Seu novo salário com aumento é de R${novo_salario:.2f}")
